e8f2f7161c3fedea08ea6532df2164754704f722
Akonicki26 <anne.konicki@mymail.champlain.edu>
2024-04-25 10:13:21 -0400

    Finish deployment setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" */}
+        <p>
+          Hi there! There's nothing here at the moment. Eventually, things will be here. Come back when that happens
+        </p>
+        <a
+          className="githib-link"
+          href="https://github.com/AKonicki26"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My GitHub profile
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
